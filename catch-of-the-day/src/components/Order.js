bc71d961757581bcb246062ed86f69b75863c110
okeeffeme <okeeffeme@gmail.com>
2017-09-01 14:45:41 +0200

    Up to step 6

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div className=''>
+        <p>Order</p>
+      </div>
+    )
+  }
+}
+
+export default Order;
