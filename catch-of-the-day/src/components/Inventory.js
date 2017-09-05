498d66153b1fdee07802f86f3c5f8f826ee6ef30
okeeffeme <okeeffeme@gmail.com>
2017-09-05 20:19:48 +0200

    Up to begining of 17

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,6 +7,7 @@ class Inventory extends React.Component {
       <div className=''>
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishies</button>
       </div>
     )
   }
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if(isAvailable) {
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal;
+    }, 0);
     return (
-      <div className=''>
-        <p>Order</p>
+      <div className='order-wrap'>
+        <h2>Your Order</h2>
+        <p>{orderIds}</p>
+        {total}
       </div>
     )
   }
