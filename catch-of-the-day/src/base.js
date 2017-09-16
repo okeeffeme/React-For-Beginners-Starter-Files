4b69b7a06007e64b8fe228ea94fa9692e79bf6f5
okeeffeme <okeeffeme@gmail.com>
2017-09-16 14:33:03 +0200

    Up to lesson 19

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCrxDpugUpyUn-LsVj9B1OIauGBNUQEySI",
+  authDomain: "catch-of-the-day-mok.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mok.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -19,6 +20,33 @@ class App extends React.Component {
       order: {}
     };
   }
+
+  componentWillMount() {
+    //this runs right before the App is rendered
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+
+    //check if there is an order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    if(localStorageRef) {
+      //update App component's order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  }
+
+  componentWillUnmount() {
+      base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+    JSON.stringify(nextState.order));
+  }
+
   addFish(fish) {
     //update our state
     const fishes = {...this.state.fishes};
@@ -61,7 +89,11 @@ class App extends React.Component {
               }
             </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params}
+        />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
